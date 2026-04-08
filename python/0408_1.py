960ac5f152971cea16f74c1c698cd4f3464a4b15
midinko334 <simakaze564@gmail.com>
2026-04-08 13:57:54 +0900

    a

---
+++ python/0408_1.py
@@ -0,0 +1,9 @@
+i=1
+while i<=40:
+  print(i)
+  if i%3==0:
+    print("Kisarazu",end="")
+  if i%5==0:
+    print("Kosen",end="")
+  print("\n")
+  i=i+1
---
+++ python/0408_2.py
@@ -0,0 +1,6 @@
+i=0
+j=0
+while i*3<=100:
+  j=j+i*3
+  i=i+1
+print(j)
