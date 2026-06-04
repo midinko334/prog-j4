72faf837ce923fd475235fa50eac92b279209f77
midinko334 <simakaze564@gmail.com>
2026-06-04 12:26:46 +0900

    asdf

--- msweeper/C/backup/mswp2.c
+++ msweeper/C/backup/mswp2.c
@@ -42,14 +42,7 @@
 #endif
 
 #define XY(size) ((size)*(size))
-#define CELLINIT (*cell)
 #define BOARDSIZE 50
-#define MINE -1
-#define MMINE -2
-#define QMINE -3
-#define SAFE 9
-#define MSAFE 10
-#define QSAFE 11
 
 void swap(int *a,int *b){ int c; c=*a; *a=*b; *b=c; }
 
@@ -63,8 +56,8 @@ int board_sel(int **cell,int size,int xy){
         if(j+(i*size)==xy) printf("\x1b[7m");
         if(cell[i][j]>0&&cell[i][j]<=8) printf("%d ",cell[i][j]);
         else if(cell[i][j]==0) printf("  ");
-        else if(cell[i][j]==MSAFE||cell[i][j]==MMINE) printf("M ");
-        else if(cell[i][j]==QSAFE||cell[i][j]==QMINE) printf("? ");
+        else if(cell[i][j]==10||cell[i][j]==-2) printf("M ");
+        else if(cell[i][j]==11||cell[i][j]==-3) printf("? ");
         else printf(". ");
         printf("\x1b[0m");
       }
@@ -79,16 +72,16 @@ int board_sel(int **cell,int size,int xy){
     if(input=='d'&&(xy%size)+1<size) xy++;
     if(input=='g'){ xy=-1; break;}
     if(input=='m'){
-      if(cell[xy/size][xy%size]==SAFE) cell[xy/size][xy%size]=MSAFE;
-      else if(cell[xy/size][xy%size]==MINE) cell[xy/size][xy%size]=MMINE;
-      else if(cell[xy/size][xy%size]==MSAFE) cell[xy/size][xy%size]=SAFE;
-      else if(cell[xy/size][xy%size]==MMINE) cell[xy/size][xy%size]=MINE;
+      if(cell[xy/size][xy%size]==9) cell[xy/size][xy%size]=10;
+      else if(cell[xy/size][xy%size]==-1) cell[xy/size][xy%size]=-2;
+      else if(cell[xy/size][xy%size]==10) cell[xy/size][xy%size]=9;
+      else if(cell[xy/size][xy%size]==-2) cell[xy/size][xy%size]=-1;
     }
     if(input=='q'){
-      if(cell[xy/size][xy%size]==SAFE) cell[xy/size][xy%size]=QSAFE;
-      else if(cell[xy/size][xy%size]==MINE) cell[xy/size][xy%size]=QMINE;
-      else if(cell[xy/size][xy%size]==QSAFE) cell[xy/size][xy%size]=SAFE;
-      else if(cell[xy/size][xy%size]==QMINE) cell[xy/size][xy%size]=MINE;
+      if(cell[xy/size][xy%size]==9) cell[xy/size][xy%size]=11;
+      else if(cell[xy/size][xy%size]==-1) cell[xy/size][xy%size]=-3;
+      else if(cell[xy/size][xy%size]==11) cell[xy/size][xy%size]=9;
+      else if(cell[xy/size][xy%size]==-3) cell[xy/size][xy%size]=-1;
     }
   }
 
@@ -99,17 +92,17 @@ void opencell(int **cell,int x,int y,int size){
   int count=0;
   for(int j=-1;j<2;j++) for(int k=-1;k<2;k++)
    if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size))
-   if(cell[y+j][x+k]==MINE||cell[y+j][x+k]==MMINE||cell[y+j][x+k]==QMINE) count++;
+   if(cell[y+j][x+k]==-1||cell[y+j][x+k]==-2||cell[y+j][x+k]==-3) count++;
   cell[y][x]=count;
   if(count==0) for(int j=-1;j<2;j++) for(int k=-1;k<2;k++)
-   if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size)&&(cell[y+j][x+k]==SAFE||cell[y+j][x+k]==MSAFE||cell[y+j][x+k]==QSAFE)) opencell(cell,x+k,y+j,size);
+   if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size)&&(cell[y+j][x+k]==9||cell[y+j][x+k]==10||cell[y+j][x+k]==11)) opencell(cell,x+k,y+j,size);
 }
 
 void fg_opencell(int **cell,int x,int y,int size){
   int count=0;
   for(int j=-1;j<2;j++) for(int k=-1;k<2;k++)
    if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size))
-   if(cell[y+j][x+k]==MINE||cell[y+j][x+k]==MMINE||cell[y+j][x+k]==QMINE) count++;
+   if(cell[y+j][x+k]==-1||cell[y+j][x+k]==-2||cell[y+j][x+k]==-3) count++;
   cell[y][x]=count;
 }
 
@@ -117,11 +110,11 @@ void finish_game(int **cell,int size){
     printf("\x1b[2J\x1b[H");
     for(int i=0;i<size;i++){
       for(int j=0;j<size;j++){
-        if(cell[i][j]==SAFE||cell[i][j]==QSAFE) fg_opencell(cell,j,i,size);
+        if(cell[i][j]==9||cell[i][j]==11) fg_opencell(cell,j,i,size);
         if(cell[i][j]>0&&cell[i][j]<=8) printf("%d ",cell[i][j]);
-        else if(cell[i][j]==MSAFE) printf("\x1b[31mM \x1b[39m");
+        else if(cell[i][j]==10) printf("\x1b[31mM \x1b[39m");
         else if(cell[i][j]==0) printf("  ");
-        else if(cell[i][j]==MINE||cell[i][j]==MMINE||cell[i][j]==QMINE) printf("* ");
+        else if(cell[i][j]==-1||cell[i][j]==-2||cell[i][j]==-3) printf("* ");
         else printf(". ");
       }
       printf("\n");
@@ -133,7 +126,7 @@ void gameloop(int **cell,int size,int xy){
   int finflag=2,clflag=0,x,y;
   while(1){
     clflag=1;
-    for(int i=0;i<size;i++) for(int j=0;j<size;j++) if(cell[i][j]==SAFE||cell[i][j]==MSAFE||cell[i][j]==QSAFE) clflag=0;
+    for(int i=0;i<size;i++) for(int j=0;j<size;j++) if(cell[i][j]==9||cell[i][j]==10||cell[i][j]==11) clflag=0;
     if(clflag){
       finflag=1;
       break;
@@ -145,22 +138,22 @@ void gameloop(int **cell,int size,int xy){
     x=xy%(size);
     y=xy/(size);
 
-    if(cell[y][x]==SAFE){
+    if(cell[y][x]==9){
       opencell(cell,x,y,size);
     }
     if(cell[y][x]>=1&&cell[y][x]<=8){
       int count=0;
       for(int j=-1;j<2;j++) for(int k=-1;k<2;k++)
        if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size))
-       if(cell[y+j][x+k]==MSAFE||cell[y+j][x+k]==QSAFE||cell[y+j][x+k]==MMINE||cell[y+j][x+k]==QMINE) count++;
+       if(cell[y+j][x+k]==10||cell[y+j][x+k]==11||cell[y+j][x+k]==-2||cell[y+j][x+k]==-3) count++;
       if(count==cell[y][x]) for(int j=-1;j<2;j++) for(int k=-1;k<2;k++)
        if((x+k>=0&&x+k<size)&&(y+j>=0&&y+j<size)){
-        if(cell[y+j][x+k]==SAFE) opencell(cell,x+k,y+j,size);
-        else if(cell[y+j][x+k]==MINE) finflag=0;
+        if(cell[y+j][x+k]==9) opencell(cell,x+k,y+j,size);
+        else if(cell[y+j][x+k]==-1) finflag=0;
       }
       if(finflag==0) break;
     }
-    else if(cell[y][x]==MINE){
+    else if(cell[y][x]==-1){
       finflag=0;
       break;
     }
@@ -171,21 +164,35 @@ void gameloop(int **cell,int size,int xy){
   else printf("*** GAME END ***\n");
 }
 
-int setup(int ***cell,int mine,int size){
+void game_start(){
+  int size=0;
+  int mine=0,c,*senkei;
+  printf("=== M Sweeper ===\n");
+  while(size<2||size>BOARDSIZE){
+    printf("Board size (Max %d Min 2): ",BOARDSIZE);
+    scanf("%d",&size);
+    while((c = getchar()) != '\n' && c != EOF);
+    if(size<2||size>BOARDSIZE) printf("Invalid Input\n");
+  }
+  while(mine>=XY(size)||mine<=0){
+    printf("Mine count : ");
+    scanf("%d",&mine);
+    while((c = getchar()) != '\n' && c != EOF);
+    if(mine>=XY(size)||mine<=0) printf("Invalid Input\n");
+  }
 
-  int *senkei;
   srand((unsigned)time(NULL));
   //0-8:opened -1:mine 9:safe -2:mine(marked) 10:safe(marked) -3:mine(q) 11:safe(q)
-  CELLINIT=(int**)malloc(sizeof(int*)*(size));
-  for(int i=0;i<size;i++) CELLINIT[i]=(int*)malloc(sizeof(int)*(size));
+  int **cell=(int**)malloc(sizeof(int*)*(size));
+  for(int i=0;i<size;i++) cell[i]=(int*)malloc(sizeof(int)*(size));
   senkei=(int*)malloc(sizeof(int)*XY(size));
 
-  for(int i=0;i<size;i++) for(int j=0;j<size;j++) CELLINIT[i][j]=SAFE;
+  for(int i=0;i<size;i++) for(int j=0;j<size;j++) cell[i][j]=9;
   for(int i=0;i<XY(size);i++) senkei[i]=i;
   for(int i=XY(size)-1;i>0;i--) swap(senkei+( rand()%(i+1) ),senkei+i);
 
   int input;
-  input=board_sel(CELLINIT,size,0);
+  input=board_sel(cell,size,0);
 
   int cur=0;
   for(int i=0;i<XY(size)-2-cur;i++){
@@ -204,38 +211,15 @@ int setup(int ***cell,int mine,int size){
     xy=senkei[i];
     x=xy%(size);
     y=xy/(size);
-    CELLINIT[y][x]=-1;
+    cell[y][x]=-1;
   }
 
   x=input%(size);
   y=input/(size);
-  opencell(CELLINIT,x,y,size);
+  opencell(cell,x,y,size);
 
   free(senkei);
-
-  return input;
-
-}
-
-void game_start(){
-  int size=0,mine=0;
-  int c,**cell;
-  printf("=== M Sweeper ===\n");
-  while(size<2||size>BOARDSIZE){
-    printf("Board size (Max %d Min 2): ",BOARDSIZE);
-    scanf("%d",&size);
-    while((c = getchar()) != '\n' && c != EOF);
-    if(size<2||size>BOARDSIZE) printf("Invalid Input\n");
-  }
-  while(mine>=XY(size)||mine<=0){
-    printf("Mine count : ");
-    scanf("%d",&mine);
-    while((c = getchar()) != '\n' && c != EOF);
-    if(mine>=XY(size)||mine<=0) printf("Invalid Input\n");
-  }
-
-  int sttxy=setup(&cell,mine,size);
-  gameloop(cell,size,sttxy);
+  gameloop(cell,size,input);
   for(int i=0;i<size;i++) free(cell[i]);
   free(cell);
 
